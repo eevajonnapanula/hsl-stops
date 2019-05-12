be8470060d50605dfb1eecc560c9c44cc5a9da41
eevajonnapanula <eevajonna.panula@gmail.com>
2019-05-12 09:26:50 +0300

    Change divs to right elements, fix some strange things

--- src/components/address-search.js
+++ src/components/address-search.js
@@ -1,35 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import { StyledInput } from './styles'
-import { getJSON } from './networking'
-import { ListContainer, ListItem, SearchContainer } from './styles'
+import React, { useState, useEffect } from "react"
+import { StyledInput } from "./styles"
+import { getJSON } from "./networking"
+import { ListContainer, ListItem, SearchContainer } from "./styles"
 
 const AddressSearch = ({ handleChange }) => {
-  const [ suggestions, setSuggestions ] = useState([])
-  const [ queryString, setQueryString ] = useState('')
-  const [ radius, setRadius ] = useState(500)
-  const [ search, setSearch ] = useState('')
-  const [ pose, setPose ] = useState('closed') 
-
-  
+  const [suggestions, setSuggestions] = useState([])
+  const [queryString, setQueryString] = useState("")
+  const [radius, setRadius] = useState(500)
+  const [search, setSearch] = useState("")
+  const [pose, setPose] = useState("closed")
 
   useEffect(() => {
     const handleAddressChange = async () => {
       if (search.length > 2) {
-      const url = `https://api.digitransit.fi/geocoding/v1/autocomplete?text=${search}&focus.point.lat=60.17&focus.point.lon=24.93`
-      const res = await getJSON(url)
+        const url = `https://api.digitransit.fi/geocoding/v1/autocomplete?text=${search}&focus.point.lat=60.17&focus.point.lon=24.93`
+        const res = await getJSON(url)
 
-      setSuggestions(res.features)
-      setPose('open')
+        setSuggestions(res.features)
+        setPose("open")
       }
-  }
+    }
     handleAddressChange()
   }, [search])
 
-
-
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      setPose('closed')
+  const handleKeyPress = e => {
+    if (e.key === "Enter") {
+      setPose("closed")
       setSearch(queryString)
     }
   }
@@ -47,13 +43,14 @@ const AddressSearch = ({ handleChange }) => {
       lat: suggestion.geometry.coordinates[1],
       radius: radius
     })
-    setPose('closed')
+    setPose("closed")
     setSuggestions([])
-    setQueryString('')
+    setQueryString("")
   }
 
   return (
-    <SearchContainer>
+    <section>
+      <SearchContainer>
         <h5>Search by address</h5>
         <StyledInput
           value={queryString}
@@ -61,32 +58,35 @@ const AddressSearch = ({ handleChange }) => {
           placeholder="Type an address"
           onKeyPress={handleKeyPress}
         />
-      <div>
-        <span>Change radius</span>
-        <StyledInput
-          placeholder="500"
-          width="5em"
-          type="number"
-          value={radius}
-          onChange={(e) => setRadius(e.target.value)}
-          onKeyPress={handleKeyPress}
+        <div>
+          <span>Change radius</span>
+          <StyledInput
+            placeholder="500"
+            width="5em"
+            type="number"
+            value={radius}
+            onChange={e => setRadius(e.target.value)}
+            onKeyPress={handleKeyPress}
           />
-        <span>m</span>
-      </div>
-        <ListContainer visible={suggestions.length > 0} pose={pose}>
-          { suggestions.map(suggestion => (
-              <ListItem
-                key={suggestion.properties.id}
-                onClick={() => handleClick(suggestion)}>
-                {suggestion.properties.label}
-              </ListItem>
-            )
-          )}
-        </ListContainer>
+          <span>m</span>
+        </div>
       </SearchContainer>
+      {suggestions.length > 0 ? (
+        <ListContainer pose={pose}>
+          {suggestions.map(suggestion => (
+            <ListItem
+              key={suggestion.properties.id}
+              onClick={() => handleClick(suggestion)}
+            >
+              {suggestion.properties.label}
+            </ListItem>
+          ))}
+        </ListContainer>
+      ) : (
+        ""
+      )}
+    </section>
   )
-
 }
-
 
 export default AddressSearch
